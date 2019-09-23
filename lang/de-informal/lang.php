162a5a6f11c7f285551f9af4b6f0aaa6b815f78d
Remo Scheidegger <mrremo@mailbox.org>
2019-09-23 18:15:13 +0200

    First Commit

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['__background_site__']   = 'Farbe für den Seitenhintergrund (hinter dem Inhaltsbereich)';
+$lang['__link__']              = 'Allgemeine Linkfarbe';
+$lang['__existing__']          = 'Farbe für Links zu existierenden Seiten';
+$lang['__missing__']           = 'Farbe für Links zu nicht-existierenden Seiten';
+$lang['__site_width__']        = 'Breite der ganzen Seite (kann eine beliebige Längeneinheit sein: %, px, em, ...)';
+$lang['__sidebar_width__']     = 'Breite der Seitenleiste, falls vorhanden (kann eine beliebige Längeneinheit sein: %, px, em, ...)';
+$lang['__tablet_width__']      = 'Unter dieser Fensterbreite wechselt die Seite in den Tabletmodus';
+$lang['__phone_width__']       = 'Unter dieser Fensterbreite wechselt die Seite in den Handymodus';
+$lang['__pre_text__']          = 'Farbe für den Text der Inline Code Formatierung';
+$lang['__pre_background__']    = 'Farbe für den Hintergrund der Inline Code Formatierung';
+
+/* Dark */
+$lang['__text_dark__']              = 'Haupttextfarbe im dunklen Farbschema';
+$lang['__background_dark__']        = 'Haupthintergrundfarbe im dunklen Farbschema';
+$lang['__text_alt_dark__']          = 'Alternative Textfarbe im dunklen Farbschema';
+$lang['__background_alt_dark__']    = 'Alternative Hintergrundfarbe im dunklen Farbschema';
+$lang['__text_neu_dark__']          = 'Neutrale Textfarbe im dunklen Farbschema';
+$lang['__background_neu_dark__']    = 'Neutrale Hintergrundfarbe im dunklen Farbschema';
+$lang['__border_dark__']            = 'Rahmenfarbe im dunklen Farbschema';
+$lang['__highlight_dark__']         = 'Hervorhebungsfarbe (hauptsächlich für Suchergebnisse) im dunklen Farbschema';
+$lang['__background_site_dark__']   = 'Farbe für den Seitenhintergrund (hinter dem Inhaltsbereich) im dunklen Farbschema';
+$lang['__pre_text_dark__']          = 'Farbe für den Text der Inline Code Formatierung im dunklen Farbschema';
+$lang['__pre_background_dark__']    = 'Farbe für den Hintergrund der Inline Code Formatierung im dunklen Farbschema';
+$lang['__link_dark__']              = 'Allgemeine Linkfarbe im dunklen Farbschema';
+$lang['__existing_dark__']          = 'Farbe für Links zu existierenden Seiten im dunklen Farbschema';
+$lang['__missing_dark__']           = 'Farbe für Links zu nicht-existierenden Seiten im dunklen Farbschema';
---
+++ lang/en/lang.php
@@ -0,0 +1,31 @@
+<?php
+
+// style.ini values
+
+$lang['__background_site__'] = 'Color for the very background (behind the content box)';
+$lang['__link__']     = 'The general link color';
+$lang['__existing__'] = 'The color for links to existing pages';
+$lang['__missing__']  = 'The color for links to non-existing pages';
+$lang['__site_width__']    = 'The width of the full site (can be any length unit: %, px, em, ...)';
+$lang['__sidebar_width__'] = 'The width of the sidebar, if any (can be any length unit: %, px, em, ...)';
+$lang['__tablet_width__']  = 'Below screensizes of this width, the site switches to tablet mode';
+$lang['__phone_width__']   = 'Below screensizes of this width, the site switches to phone mode';
+$lang['__theme_color__']   = 'Theme color of the web app';
+$lang['__pre_text__']       = 'Color for inline code text formatting';
+$lang['__pre_background__'] = 'Color for the background of the inline code formatting';
+
+/* Dark */
+$lang['__text_dark__'] = 'Main text color in dark color scheme';
+$lang['__background_dark__'] = 'Main background color in dark color scheme';
+$lang['__text_alt_dark__'] = 'Alternative text color in dark color scheme';
+$lang['__background_alt_dark__'] = 'Alternative background color in dark color scheme';
+$lang['__text_neu_dark__'] = 'Neutral text color in dark color scheme';
+$lang['__background_neu_dark__'] = 'Neutral background color in dark color scheme';
+$lang['__border_dark__'] = 'Frame color in dark color scheme';
+$lang['__highlight_dark__'] = 'Highlight color (mainly for search results) in dark color scheme';
+$lang['__background_site_dark__'] = 'Color for page background (behind content area) in dark color scheme';
+$lang['__pre_text_dark__'] = 'Color for inline code text formatting in dark color scheme';
+$lang['__pre_background_dark__'] = 'Color for the background of the inline code formatting in the dark color scheme';
+$lang['__link_dark__'] = 'General link color in dark color scheme';
+$lang['__existing_dark__'] = 'Color for links to existing pages in dark color scheme';
+$lang['__missing_dark__'] = 'Color for links to non-existing pages in dark color scheme';
