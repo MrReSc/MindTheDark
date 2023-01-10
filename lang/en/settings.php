d0999bb3d112d423d8cbb0c492707063428e3ddf
MrReSc <resc@mailbox.org>
2023-01-10 06:09:28 +0100

    clean up

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * Deutsche Übersetzung Einstellungen
- *
- */
 
 $lang['theme']      = 'Color scheme if autoDark is disabled.';
 $lang['autoDark']   = 'Automatic detection of the color scheme by the operating system';
 $lang['userChoice'] = 'Activates a button so that the user can switch the color scheme himself.';
 $lang['pluginNote'] = 'Activate support for the Note plugin.';
-$lang['pluginWrap']       = 'Activate support for the Wrap plugin.';
+$lang['pluginWrap'] = 'Activate support for the Wrap plugin.';
